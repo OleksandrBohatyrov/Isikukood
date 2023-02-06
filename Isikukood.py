557ef03c366da7f081513ad1167775dc7824acec
OleksandrBohatyrov <120181397+OleksandrBohatyrov@users.noreply.github.com>
2023-02-06 14:39:57 +0200

    Update Isikukood.py

--- Isikukood.py
+++ Isikukood.py
@@ -4,20 +4,36 @@ from omamoodul import *
 #print(isikukood_list)
 
 ikood=""
+
+
+
+arvud=[100,1001,211,222]
+ikoodid=[]
+
 while True:
     ikood=input("Sesesta isikukood: ")
     if pikkus(ikood)==False:
         print("Liig pikk või lühike isikukood")
+        arvud.append(ikood)
     else:
         s=sugu_check(ikood)
         if s=="viga":
             print("Esimene täht ei ole õige")
+            arvud.append(ikood)
         else:
             if sunnipaev(ikood)=="viga":
                 print("2-7 tähed on valesti sisestatud")
             else:
                 print(sunnipaev(ikood))
-                if sunnikoht(ikood)=="viga":
-                    print(" asd")
+                if sunnikoht(ikood)==int(ikood[-1]):
+
+                    print("OK")
+                    ikood.append(ikood)
                 else:
                     print(lause(ikood))
+                    print(kontrollnr(ikood))
+    print()
+    ikoodid=naised_mehed(ikoodid)
+    print(ikoodid)
+    arvud_sorted(arvud)
+    print(arvud)
